1959c194dfb8fd5f5042a5b4bfd0ffdd9682f0fb
BJladika <BJladika@gmail.com>
2021-12-03 15:20:39 +0900

    Basic design finished (kinda); TODO: 404, refactor CSS/HTML, redesign city details (not forecast, only info), spinner (loading progress)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-weather-multiple-cities';
+  title = 'Weather App Angular (by MaxCorp)';
 }
